adc586c33c8a9a5211f2aaa541022ccf1be2f326
Perkles <lucasbrittobr@gmail.com>
2022-07-13 16:28:56 -0300

    Version 1.0

---
+++ Import and Export/main.js
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from './script.js'
+
+console.log(mostraIdade('Lucas', 19))
+console.log(mostraCidade('Lucas', 'Cotia'))
+console.log(mostraHobby('Lucas', 'Xadres'))
